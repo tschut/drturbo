159544cffda3ab9900b35a1ddeee9b47dd3ecf9b
tschut <tschut@gmail.com>
2013-09-10 19:58:28 +0200

    Removed ads

--- Android/src/com/games/spaceman/EndLevelDialogFragment.java
+++ Android/src/com/games/spaceman/EndLevelDialogFragment.java
@@ -4,19 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.google.ads.AdRequest;
-import com.google.ads.AdSize;
-import com.google.ads.AdView;
 
 public class EndLevelDialogFragment extends DialogFragment {
     private final class OnRetryClickListener implements View.OnClickListener {
@@ -29,8 +22,10 @@ public class EndLevelDialogFragment extends DialogFragment {
 
     private final class OnLevelListClickListener implements View.OnClickListener {
         public void onClick(View v) {
-            // Reload the current level. If we don't do that, the flow makes it possible to get back to
-            // the level in the state it's in now because you can press the 'back'button in the level selector
+            // Reload the current level. If we don't do that, the flow makes it
+            // possible to get back to
+            // the level in the state it's in now because you can press the
+            // 'back'button in the level selector
             GameThreadHolder.getThread().reloadCurrentLevel();
             Intent intent = new Intent(activity, LevelSelect.class);
             activity.startActivityForResult(intent, SpaceApp.ACTIVITY_LEVELSELECT);
@@ -47,13 +42,12 @@ public class EndLevelDialogFragment extends DialogFragment {
     }
 
     private Activity activity;
-    private AdView adView;
-    private int textResource;
-    private int titleResource;
-    private int imageResource;
-    private int best;
-    private int points;
-    private boolean nextLevelUnlocked;
+    private int      textResource;
+    private int      titleResource;
+    private int      imageResource;
+    private int      best;
+    private int      points;
+    private boolean  nextLevelUnlocked;
 
     public void setStartingActivity(Activity activity) {
         this.activity = activity;
@@ -69,17 +63,6 @@ public class EndLevelDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.end_level_layout, container);
-
-        LinearLayout adLayout = (LinearLayout) view.findViewById(R.id.adLayout);
-
-        adView = new AdView(activity, AdSize.BANNER, AdSettings.ADSENSE_ID);
-        DisplayMetrics displayMetrics = activity.getResources().getDisplayMetrics();
-        int width = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, AdSize.BANNER.getWidth(), displayMetrics));
-        int height = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, AdSize.BANNER.getHeight(), displayMetrics));
-        adLayout.addView(adView, width, height);
-        AdRequest adRequest = new AdRequest();
-        adRequest.addTestDevice(AdSettings.TEST_DEVICE_ID);
-        adView.loadAd(adRequest);
 
         getDialog().setTitle(titleResource);
         ((TextView) view.findViewById(R.id.end_level_subtitle)).setText(textResource);
@@ -100,16 +83,7 @@ public class EndLevelDialogFragment extends DialogFragment {
         return view;
     }
 
-    @Override
-    public void onDestroy() {
-        if (adView != null) {
-            adView.destroy();
-        }
-        super.onDestroy();
-    }
-
-    public void setProperties(int points, int best, int imageResource, int titleResource, int textResource,
-            boolean nextLevelUnlocked) {
+    public void setProperties(int points, int best, int imageResource, int titleResource, int textResource, boolean nextLevelUnlocked) {
         this.points = points;
         this.best = best;
         this.imageResource = imageResource;
--- Android/src/com/games/spaceman/PauseMenuFragment.java
+++ Android/src/com/games/spaceman/PauseMenuFragment.java
@@ -4,17 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
-import com.google.ads.AdRequest;
-import com.google.ads.AdSize;
-import com.google.ads.AdView;
 import com.spacemangames.framework.SpaceGameState;
 
 public class PauseMenuFragment extends DialogFragment {
@@ -28,8 +22,10 @@ public class PauseMenuFragment extends DialogFragment {
 
     private final class OnLevelListClickListener implements View.OnClickListener {
         public void onClick(View v) {
-            // Reload the current level. If we don't do that, the flow makes it possible to get back to
-            // the level in the state it's in now because you can press the 'back'button in the level selector
+            // Reload the current level. If we don't do that, the flow makes it
+            // possible to get back to
+            // the level in the state it's in now because you can press the
+            // 'back'button in the level selector
             int lState = SpaceGameState.getInstance().endState();
             if (lState != SpaceGameState.NOT_YET_ENDED) {
                 GameThreadHolder.getThread().reloadCurrentLevel();
@@ -48,7 +44,6 @@ public class PauseMenuFragment extends DialogFragment {
     }
 
     private Activity activity;
-    private AdView adView;
 
     public void setStartingActivity(Activity activity) {
         this.activity = activity;
@@ -57,16 +52,6 @@ public class PauseMenuFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.pause_layout, container);
-        LinearLayout adLayout = (LinearLayout) view.findViewById(R.id.adLayout);
-
-        adView = new AdView(activity, AdSize.BANNER, AdSettings.ADSENSE_ID);
-        DisplayMetrics displayMetrics = activity.getResources().getDisplayMetrics();
-        int width = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, AdSize.BANNER.getWidth(), displayMetrics));
-        int height = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, AdSize.BANNER.getHeight(), displayMetrics));
-        adLayout.addView(adView, width, height);
-        AdRequest adRequest = new AdRequest();
-        adRequest.addTestDevice(AdSettings.TEST_DEVICE_ID);
-        adView.loadAd(adRequest);
 
         getDialog().setTitle(R.string.pause_title);
 
@@ -79,13 +64,5 @@ public class PauseMenuFragment extends DialogFragment {
         pauseRestart.setOnClickListener(new OnRestartClickListener());
 
         return view;
-    }
-
-    @Override
-    public void onDestroy() {
-        if (adView != null) {
-            adView.destroy();
-        }
-        super.onDestroy();
     }
 }
